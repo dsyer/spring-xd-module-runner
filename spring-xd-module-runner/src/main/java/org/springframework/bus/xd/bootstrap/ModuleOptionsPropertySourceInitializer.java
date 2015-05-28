ca4d6efde715dd27ec176202070e36463a96dad6
Dave Syer <dsyer@pivotal.io>
2015-05-28 09:28:00 +0100

    Switch to org.springframework.bus packaging

--- spring-xd-module-runner/src/main/java/org/springframework/bus/xd/bootstrap/ModuleOptionsPropertySourceInitializer.java
+++ spring-xd-module-runner/src/main/java/org/springframework/bus/xd/bootstrap/ModuleOptionsPropertySourceInitializer.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.xd.module.runner.bootstrap;
+package org.springframework.bus.xd.bootstrap;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
